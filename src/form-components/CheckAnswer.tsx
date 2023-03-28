735c5332c2183fdf0af45575fade6abc76e4ee1b
rachel-sison <RSISON@UDEL.EDU>
2023-03-27 21:48:41 -0400

    solved forms `

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,37 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    //This is the State (Model)
+    const [givenAnswer, setGivenAnswer] = useState<string>("");
+
+    //This is the Control
+    function updateCorrectness(event: ChangeEvent) {
+        setGivenAnswer(event.target.value);
+    }
+
+    //This is the View
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="formAnswer">
+                <Form.Label>Type your answer:</Form.Label>
+                <Form.Control
+                    value={givenAnswer}
+                    onChange={updateCorrectness}
+                />
+            </Form.Group>
+            <div>
+                Your answer is {givenAnswer === expectedAnswer ? "✔️" : "❌"}
+            </div>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +12,27 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    //This is the State (Model)
+    const [choice, setChoice] = useState<string>("");
+
+    //This is the Control
+    function updateChoice(event: ChangeEvent) {
+        setChoice(event?.target.value);
+    }
+
+    //This is the View
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="userChoice">
+                <Form.Label>Choose your answer:</Form.Label>
+                <Form.Select value={choice} onChange={updateChoice}>
+                    {options.map((myChoice: string) => (
+                        <option key={myChoice}>{myChoice}</option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div>Your answer is {choice === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
